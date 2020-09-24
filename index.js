d41b1805c923356707c08f52bf95026c778b7945
Aliaksei Makas <a_makas@wargaming.net>
2020-09-24 21:33:41 +0300

    fix: return webhook

--- index.js
+++ index.js
@@ -7,6 +7,11 @@ const { clearCallback, saveMessageId } = require('./src/clear')
 const { setTimer } = require('./src/timeout')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
+const PORT = process.env.PORT || 3000;
+const URL = process.env.URL
+
+bot.telegram.setWebhook(`${URL}/bot${process.env.BOT_TOKEN}`);	bot.command('clear', clearCallback)
+bot.startWebhook(`/bot${process.env.BOT_TOKEN}`, null, PORT)
 
 bot.command('clear', clearCallback)
 bot.command('timeout', setTimer)
